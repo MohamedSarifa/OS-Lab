adb5742097c67c6c53bdaac6f295150d43a8343a
M.MOHAMED SARIFA <mohamedsarifa2007@gmail.com>
2026-08-20 10:07:15 +0530

    Update mes1producer.c

--- EX4/mes1producer.c
+++ EX4/mes1producer.c
@@ -14,30 +14,19 @@ int main()
     key_t key;
     int msgid;
     struct message msg;
-    int i, count = 0;
 
     key = ftok("msgfile", 65);
 
     msgid = msgget(key, 0666 | IPC_CREAT);
 
-    msgrcv(msgid, &msg, sizeof(msg.msg_text), 1, 0);
+    msg.msg_type = 1;
 
-    printf("Message received: %s", msg.msg_text);
+    printf("Enter a message: ");
+    fgets(msg.msg_text, 100, stdin);
 
-    for(i = 0; msg.msg_text[i] != '\0'; i++)
-    {
-        if(msg.msg_text[i] != ' ' &&
-           (msg.msg_text[i + 1] == ' ' ||
-            msg.msg_text[i + 1] == '\n' ||
-            msg.msg_text[i + 1] == '\0'))
-        {
-            count++;
-        }
-    }
+    msgsnd(msgid, &msg, sizeof(msg.msg_text), 0);
 
-    printf("Number of words = %d\n", count);
-
-    msgctl(msgid, IPC_RMID, NULL);
+    printf("Message sent successfully.\n");
 
     return 0;
 }
